23385f8f07f10f271aabebef8de452c1dc1c67de
Youssef <ym600016@gmail.com>
2025-03-04 18:46:09 +0200

    fixing

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-const authenticate = async (req, res, next) => {
-  const credentials = auth(req);
-  if (!credentials) return res.status(401).json({ error: "Unauthorized" });
+// const authenticate = async (req, res, next) => {
+//   const credentials = auth(req);
+//   if (!credentials) return res.status(401).json({ error: "Unauthorized" });
 
-  const user = await prisma.user.findUnique({
-    where: { username: credentials.name },
-  });
+//   const user = await prisma.user.findUnique({
+//     where: { username: credentials.name },
+//   });
 
-  if (!user || !(await bcrypt.compare(credentials.pass, user.password))) {
-    return res.status(401).json({ error: "Invalid credentials" });
-  }
+//   if (!user || !(await bcrypt.compare(credentials.pass, user.password))) {
+//     return res.status(401).json({ error: "Invalid credentials" });
+//   }
 
-  next();
-};
+//   next();
+// };
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Express API!");
