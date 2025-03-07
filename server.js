77313c86198a4fbfa0dc2cbd5725441cb6c681a1
Youssef <ym600016@gmail.com>
2025-03-07 16:58:39 +0200

    testig gc database

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-// const authenticate = async (req, res, next) => {
-//   const credentials = auth(req);
-//   if (!credentials) return res.status(401).json({ error: "Unauthorized" });
-
-//   const user = await prisma.user.findUnique({
-//     where: { username: credentials.name },
-//   });
-
-//   if (!user || !(await bcrypt.compare(credentials.pass, user.password))) {
-//     return res.status(401).json({ error: "Invalid credentials" });
-//   }
-
-//   next();
-// };
-
 const authenticate = async (req, res, next) => {
   const credentials = auth(req);
   if (!credentials) return res.status(401).json({ error: "Unauthorized" });
